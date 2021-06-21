5f55f6cb3ee1a24d77bfed33ba65eeb1b05e7bbe
Aaron Matsumoto <aaronymatsumoto@gmail.com>
2021-06-20 19:03:28 -0700

    Add ability to mark the board

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const gameBoard = (() => {
-  const board = ["", "X", "O", "3", "4", "5", "6", "7", "8"];
+  const board = ["", "", "", "", "", "", "", "", ""];
   const getField = (index) => board[index];
   const setField = (index, value) => board[index] = value;
   const newBoard = () => ["", "", "", "", "", "", "", "", ""];
@@ -12,28 +12,57 @@ const player = (symbol) => {
 
 const displayBoard = () => {
   const boardContainer = document.getElementById('boardContainer');
+  const place = (field) => {
+    if(gameBoard.getField(field) === ""){
+      gameBoard.setField(field, "O"); //update this
+      clearBoard();
+      displayBoard();
+    }
+    else {
+      return;
+    }
+  }
+
   for (let i = 0; i < 9; i++){
     let field = document.createElement("div");
 		field.classList = "field";
     field.id = `field${i}`;
     field.innerHTML = gameBoard.getField(i);
+    field.onclick = function () {
+      place(i);
+    }
 		boardContainer.appendChild(field);
   }
-  console.log(`done display board`);
 }
 
-/*
-const place = (field) => {
 
+//removes all board elements
+const clearBoard = () => {
+  const allFields = document.getElementsByClassName('field');
+  while(allFields[0]) {
+    allFields[0].parentNode.removeChild(allFields[0]);
+  }
 }
-*/
 
-//console.log(gameBoard.getField(4));
-gameBoard.setField(0, "X");
-console.log(gameBoard.board);
 
-const playerOne = player("X");
-const playerTwo = player("O");
-console.log(playerOne.symbol);
 
-displayBoard();
+// to play the game
+const displayController = (() => {
+  //player one enter name
+  const playerOneName = "Aaron";
+  const playerOne = player("X", playerOneName);
+  //player two enter name or select AI
+  const playerTwoName = "Trinh";
+  const playerTwo = player("O", playerOneName);
+  //randomize who goes first
+  //for now player 1 goes first
+  let currentTurn = playerOneName;
+
+  //change turns
+
+  gameBoard.setField(0, "X");
+  console.log(gameBoard.board);
+  console.log(playerOne.symbol);
+
+  displayBoard();
+})();
